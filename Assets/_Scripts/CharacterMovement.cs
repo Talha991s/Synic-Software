9f4210e5dafe477af073af58395ef3474ae6ca15
Talha991s <55731371+Talha991s@users.noreply.github.com>
2022-01-19 00:45:30 -0800

    Player movement

---
+++ Assets/_Scripts/CharacterMovement.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMovement : MonoBehaviour
+{
+    public CharacterController controller;
+    public Transform cam;
+
+    public float speed = 6.0f;
+
+    public float turnSmoothTime = 0.1f;
+
+    public float turnsmoothVelocity;
+
+    Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if(direction.magnitude >= 0.1f)
+        {
+            anim.SetInteger("AnimParam", 1);
+            float targetAngle = Mathf.Atan2(direction.x, direction.y) *Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnsmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0.0f, angle, 0.0f);
+
+            Vector3 moveDir = Quaternion.Euler(0.0f, targetAngle, 0.0f) * Vector3.forward;
+
+            controller.Move(moveDir.normalized * speed * Time.deltaTime);
+        }
+        else
+        {
+            anim.SetInteger("AnimParam", 0);
+        }
+
+
+    }
+}
